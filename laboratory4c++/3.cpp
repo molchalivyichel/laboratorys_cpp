91bfe1e318d4365aba86f8637ad5ad87fca0d4e9
Molchalivyy chel <133192990+molchalivyichel@users.noreply.github.com>
2024-11-06 13:06:27 +0300

    Update 3.cpp

--- laboratory4c++/3.cpp
+++ laboratory4c++/3.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main()
 {
     int size = 10;
-    int const massive[size] = {0};
+    int massive[size] = {0,1,2,3,4,5,6,7,8,9};
+    const int* first_element = massive;
     
     for(int i = 0; i < size; i++)
     {
-        cout << *(massive + i) << " ";
+        cout << *first_element++ << " ";
     }
     
     return 0;
